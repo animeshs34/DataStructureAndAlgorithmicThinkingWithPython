8f3ef950dc75b5971998e871d9656dc7ee361c92
animeshs34 <46777236+animeshs34@users.noreply.github.com>
2020-03-25 19:48:24 +0530

    aaaa

--- src/chapter01introduction/ComplexityWithBreakStatement.py
+++ src/chapter01introduction/ComplexityWithBreakStatement.py
@@ -11,14 +11,14 @@
 
 def function(n):
 	count = 0
-	for i in range(n / 2, n):  # Outer loop execute n/2 times
+	for i in range(n // 2, n):  # Outer loop execute n/2 times
 		j = 1
 		count = count + 1
 		while j + n / 2 <= n:  # Middle loop has break
 			break
 			j = j * 2
 			count = count + 1
-			
+
 	print (count)
 
 function(20)
